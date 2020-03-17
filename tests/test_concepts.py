1a2ebb457c6dcbeb2c1665fc6f135df4a5dd263b
JeremyLecoeur <jeremy@blackfynn.com>
2020-03-17 10:25:30 -0400

    reverting changes

--- tests/test_concepts.py
+++ tests/test_concepts.py
@@ -469,7 +469,7 @@ def test_related_records_pagination(dataset):
     # Get all records
     gotten = fred.get_related()
     assert len(gotten) == 200
-    assert sorted([int(r.get("field")) for r in gotten]) == list(range(200))
+    assert [int(r.get("field")) for r in gotten] == list(range(200))
 
 def test_stringified_boolean_values(dataset):
     ppatient = dataset.create_model(
